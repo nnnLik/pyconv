42ddedd5b153ecd01f29aac130c2531b52def761
nnnLik <rslmakhmudov@gmail.com>
2023-08-31 13:55:58 +0300

    [ADD] converters obj

---
+++ pyconv/converters.py
@@ -0,0 +1,8 @@
+from imageconverter import ImageConverter
+from audioconverter import AudioConverter
+from documentconverter import DocumentConverter
+
+
+image_converter: ImageConverter = ImageConverter()
+audio_converter: AudioConverter = AudioConverter()
+document_converter: DocumentConverter = DocumentConverter()
